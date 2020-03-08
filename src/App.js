38e6f61b9fffdf3e1ed0c49901fcdebf08ec0237
Fagley Ali Hossain <fagleyali@Fagleys-MBP.lan>
2020-03-08 12:11:26 -0500

    Sorting implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Movies from "./components/movies";
 import Pagination from "./components/pagination";
 import Genre from "./components/genres";
 import Paginate from "./utils/paginate";
+import _ from 'lodash';
 import "./App.css";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     movies: getMovies(),
     pages: 4,
     currentPage: 1,
-    currentGenre: "1"
+    currentGenre: "1",
+    sortColumn: {col:'title', order:'asc'}
   };
 
   const [state, setState] = useState(initialState);
@@ -30,11 +32,19 @@ function App() {
     setState({ ...state, currentPage: e });
   };
 
+  const handleSort = (sortColumn) => {
+    setState({...state, sortColumn})
+    console.log(state.sortColumn)
+  }
+
   const filtered =
     state.currentGenre === "1"
       ? state.movies
       : state.movies.filter(m => m.genre._id === state.currentGenre);
-  const movies = Paginate(filtered, state.currentPage, state.pages);
+  
+  const sorted = _.orderBy(filtered, [state.sortColumn.col], [state.sortColumn.order])
+      
+  const movies = Paginate(sorted, state.currentPage, state.pages);
 
   const handleGenre = selection => {
     setState({ ...state, currentGenre: selection, currentPage: 1 });
@@ -50,6 +60,8 @@ function App() {
           totalMovies={filtered.length}
           onDelete={handleDelete}
           movies={movies}
+          onSort={handleSort}
+          sortColumn={state.sortColumn}
         />
         <Pagination
           onSelect={handleSelect}
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Movies = props => {
-  const { totalMovies, onDelete, movies } = props;
+  const { totalMovies, onDelete, movies, onSort, sortColumn } = props;
+
+  const handleSortColumn = (col) => {
+    if(sortColumn.col === col){
+      if(sortColumn.order === 'asc') sortColumn.order = 'desc';
+      else sortColumn.order = 'asc';
+    }else {
+      sortColumn.col = col;
+      sortColumn.order = 'asc';
+    }
+    return sortColumn;
+  }
 
   return (
     <div className="container">
@@ -13,11 +24,11 @@ const Movies = props => {
         >{`Showing ${totalMovies} movies in the database`}</caption>
         <thead>
           <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Genre</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Rate</th>
-            <th scope="col"></th>
+             <th onClick={() => onSort(handleSortColumn('title'))} scope="col">Title</th>
+             <th onClick={() => onSort(handleSortColumn('genre.name'))} scope="col">Genre</th>
+             <th onClick={() => onSort(handleSortColumn('numberInStock'))} scope="col">Stock</th>
+             <th onClick={() => onSort(handleSortColumn('dailyRentalRate'))} scope="col">Rate</th>
+             <th onClick={() => onSort()} scope="col"></th>
           </tr>
         </thead>
         <tbody>
